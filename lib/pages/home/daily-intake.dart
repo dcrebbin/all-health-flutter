ede1a99e88ce44b0123a93656289b90dff3e61d6
Devon Crebbin <devon@artvuu.group>
2024-07-15 16:31:32 +1000

    added more pages

---
+++ lib/pages/home/daily-intake.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class DailyIntake extends StatelessWidget {
+  const DailyIntake({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Text('Daily Intake');
+  }
+}
---
+++ lib/pages/home/vitamins.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Vitamins extends StatelessWidget {
+  const Vitamins({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Text('Vitamins');
+  }
+}
