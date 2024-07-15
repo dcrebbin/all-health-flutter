b380f73e015a456cc3d48ea23d172128188ab2e9
Devon Crebbin <devon@artvuu.group>
2024-07-15 16:31:40 +1000

    updated navigation

--- lib/pages/home/home_screen.dart
+++ lib/pages/home/home_screen.dart
@@ -1,10 +1,13 @@
 import 'package:all_health_flutter/pages/home/hormones.dart';
 import 'package:all_health_flutter/pages/home/microbiome.dart';
 import 'package:all_health_flutter/pages/home/overview.dart';
+import 'package:all_health_flutter/pages/home/vitamins.dart';
 import 'package:flutter/material.dart';
 
 import 'body_composition.dart';
+import 'daily-intake.dart';
 import 'dna_list.dart';
+import 'exercise.dart';
 import 'test_widget.dart';
 
 class HomePage extends StatefulWidget {
@@ -19,41 +22,35 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return MaterialApp(
         home: DefaultTabController(
-      length: 5,
+      length: 8,
       child: Scaffold(
         appBar: AppBar(
           title: const Text('All Health'),
         ),
-        bottomNavigationBar: Container(
-            child: TabBar(
+        bottomNavigationBar: const TabBar(
+          tabAlignment: TabAlignment.start,
+          isScrollable: true,
           tabs: [
-            Tab(
-              text: 'Overview',
-            ),
-            Tab(
-              text: 'DNA',
-            ),
-            Tab(
-              text: 'Hormones',
-            ),
-            Tab(
-              text: 'Microbiome',
-            ),
-            Tab(
-              text: 'Body Composition',
-            ),
+            Tab(text: 'Overview'),
+            Tab(text: 'Daily Intake'),
+            Tab(text: 'Exercise'),
+            Tab(text: 'Vitamins'),
+            Tab(text: 'Body Composition'),
+            Tab(text: 'Hormones'),
+            Tab(text: 'Microbiome'),
+            Tab(text: 'DNA'),
           ],
-          onTap: (index) {
-            setState(() {});
-          },
-        )),
+        ),
         body: TabBarView(
           children: [
             Overview(),
-            DnaList(),
-            Hormones(),
-            Microbiome(),
-            BodyComposition(),
+            const DailyIntake(),
+            const Exercise(),
+            const Vitamins(),
+            const BodyComposition(),
+            const Hormones(),
+            const Microbiome(),
+            const DnaList(),
           ],
         ),
       ),
